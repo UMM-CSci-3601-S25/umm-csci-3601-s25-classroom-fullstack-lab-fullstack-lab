3fd087439ff33a3a831347d8837572d6209229fd
Abhineswari <seela031@umn.edu>
2025-02-12 23:01:16 -0600

    Create todo.service.ts in the client folder
    We got the information at the user.service.ts for the client side filtering
    Co-authored-by: Armando Valdez
    <valde148@d.umn.edu>

---
+++ client/src/app/todos/todo-list.components.ts
@@ -0,0 +1,106 @@
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterLink } from '@angular/router';
+import { catchError, combineLatest, of, switchMap, tap } from 'rxjs';
+import { Todo, TodoRole } from './todo';
+import { TodoCardComponent } from './todo-card.component';
+import { TodoService } from './todo.service';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+
+
+
+@Component({
+  selector: 'app-todo-list-component',
+  templateUrl: 'todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss'],
+  providers: [],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatRadioModule,
+    TodoCardComponent,
+    MatListModule,
+    RouterLink,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+  ],
+})
+export class TodoListComponent {
+  todoOwner = signal<string | undefined>(undefined);
+  todoBody = signal<string | undefined>(undefined);
+  todoCategory = signal<string | undefined>(undefined);
+  todoStatus = signal<boolean | undefined>(undefined);
+
+  viewType = signal<'card' | 'list'>('card');
+
+  errMsg = signal<string | undefined>(undefined);
+
+
+
+  constructor(private todoService: TodoService, private snackBar: MatSnackBar) {
+
+  }
+    private todoBody$ = toObservable(this.todoBody);
+    private todoOwner$ = toObservable(this.todoOwner);
+    private todoCategory$ = toObservable(this.todoCategory);
+
+
+
+    serverFilteredTodos =
+
+      toSignal(
+        combineLatest([this.todoCategory$, this.todoOwner$]).pipe(
+
+          switchMap(([category,owner]) =>
+            this.todoService.getTodos({
+              category,
+              owner,
+            })
+          ),
+
+          catchError((err) => {
+            if (err.error instanceof ErrorEvent) {
+              this.errMsg.set(
+                `Problem in the client – Error: ${err.error.message}`
+              );
+            } else {
+              this.errMsg.set(
+                `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`
+              );
+            }
+            this.snackBar.open(this.errMsg(), 'OK', { duration: 6000 });
+
+            return of<Todo[]>([]);
+          }),
+          tap(() => {
+
+          })
+        )
+      );
+
+
+    filteredTodos = computed(() => {
+      const serverFilteredTodos = this.serverFilteredTodos();
+      return this.todoService.filterTodos(serverFilteredTodos, {
+        category: this.todoCategory(),
+        owner: this.todoOwner(),
+      });
+    });
+  }
+
